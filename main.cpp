4802afe8b5da83d220bb0d93c9827f001df2a658
Kauan Pereira <kauanteixeirapereira5@gmail.com>
2024-07-17 20:18:23 -0300

    criação da classe jogo da velha

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "jogoDeTabuleiro.hpp"
 #include "lig4.hpp"
+#include "jogoDaVelha.hpp"
 
 int main() {
     int a, b;
-    std::cin >> a >> b;
 
-    Lig4 john = Lig4(a,b);
-    john.imprimir_tabuleiro();
+    // Lig4 john = Lig4(a,b);
+    // john.imprimir_tabuleiro();
+
+    JogoDaVelha jogo = JogoDaVelha();
+    jogo.imprimir_tabuleiro();
+    
+    while(true){
+        std::cout << "Faça joagada: ";
+        std::cin >> a >> b;
+        jogo.fazer_jogada(a -1,b -1);
+    }
 
     return 0;
 }

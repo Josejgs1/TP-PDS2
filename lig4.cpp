89098e83a605f39f96b3925229267edc60566d61
Kauan Pereira <kauanteixeirapereira5@gmail.com>
2024-08-09 13:34:28 -0300

    validação dos movimentos

--- lig4.cpp
+++ lig4.cpp
@@ -6,15 +6,20 @@
 Lig4::Lig4(int linhas, int colunas) : JogoDeTabuleiro(linhas, colunas) {}
 
 void Lig4::fazer_jogada(int x){
-    if(_tabuleiro[_linhas - 1][x - 1] != 0){
-        std::cout << "Movimento inválido." << std::endl;
-    } else {
-        for(int i = 0; i < _linhas; i++){
-            if(_tabuleiro[_linhas - 1 - i][x - 1] == 0){
-                _tabuleiro[_linhas - 1 - i][x - 1] = 1;
-                break;
-            }
+
+    bool invalido = false;
+
+    for(int i = 0; i < _linhas; i++){
+        if(_tabuleiro[_linhas - 1 - i][x - 1] == 0){
+            _tabuleiro[_linhas - 1 - i][x - 1] = 1;
+            invalido = true;
+            break;
         }
-        imprimir_tabuleiro();
+    }    
+    
+    if(invalido){
+        std::cout << "Movimento inválido." << std::endl;
     }
+
+    imprimir_tabuleiro();
 }
